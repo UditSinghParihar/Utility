2eff92b839233cc5a6cca8ec3d3fc8087b36d2b8
udit <parihar.2@iitj.ac.in>
2018-11-22 12:27:25 +0530

    Added cin for fprintf

--- cin_terminal.cpp
+++ cin_terminal.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 	string name;
-	fprintf(stdout, "Hello there!!\nEnter your name.\n");
+	cout << "Hello there!!\nEnter your name.\n";
 	getline(cin, name);
-	fprintf(stdout, "Welcome %s\n", name.c_str());
+	cout << "Welcome " << name << endl;
 	return 0;
 }
