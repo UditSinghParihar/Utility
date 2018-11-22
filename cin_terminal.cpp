c805e1517022d14353b4d9f19b1ac133c94bfdb6
udit <parihar.2@iitj.ac.in>
2018-11-22 12:32:01 +0530

    Finally replacing fprintf with cout.

--- cin_terminal.cpp
+++ cin_terminal.cpp
@@ -5,14 +5,8 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 	string name;
-<<<<<<< HEAD
 	cout << "Hello there!!\nEnter your name.\n";
 	getline(cin, name);
 	cout << "Welcome " << name << endl;
-=======
-	fprintf(stdout, "Hello there!!\nEnter your name.\n");
-	getline(cin, name);
-	fprintf(stdout, "Welcome %s\n", name.c_str());
->>>>>>> 39f7bb3c55e98dbf71f860f3f966ca03aee3aa0b
 	return 0;
 }
