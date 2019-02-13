5e642a8ce3ecc6d4e6869c401a91376e120639ae
udit <parihar.2@iitj.ac.in>
2019-02-13 13:32:02 +0530

    Adding constexpr practice file.

--- constexpr_fib.cpp
+++ constexpr_fib.cpp
@@ -28,12 +28,14 @@ constexpr long int fib(int num){
 
 int main(int argc, char const *argv[]){
 	Timer timer;
-	const long int result = fib(100);
-	const long int num1 = fib(99);
-	const long int num2 = fib(98);
-	cout << "Time relapsed: " << timer.elapsed() << endl;
+	const long int result = fib(200);
+	// const long int num1 = fib(99);
+	// const long int num2 = fib(98);
+	// fprintf(stdout, "fib(99) = %ld\tfib(98) = %ld\nfib(100) = %ld\tsummation is %ld\n",
+	// 		num1, num2, result, num1+num2);
 
-	fprintf(stdout, "fib(99) = %ld\tfib(98) = %ld\nfib(100) = %ld\tsummation is %ld\n",
-			num1, num2, result, num1+num2);
+	cout << "Time relapsed: " << timer.elapsed() << endl;
+	cout << "Fibonacci value: " << result << endl;
+
 	return 0;
 }
--- pcl/find_point.cpp
+++ pcl/find_point.cpp
@@ -24,27 +24,63 @@ struct Point3D{
 	}
 };
 
-void red_colorization(PointCloudT::Ptr cloud){
+void blue_colorization(PointCloudT::Ptr cloud){
 	for(size_t index=0; index<cloud->points.size(); ++index){
-		cloud->points[index].r = 230;
+		cloud->points[index].r = 20;
 		cloud->points[index].g = 20;
-		cloud->points[index].b = 20;
+		cloud->points[index].b = 230;
 	}
 }
 
 void simple_visualize(const PointCloudT::Ptr cloud){
-	pcl::visualization::PCLVisualizer viewer("Reddening");
+	pcl::visualization::PCLVisualizer viewer("Normal_cloud");
 	pcl::visualization::PointCloudColorHandlerRGBField<PointT> rgb(cloud);
 	viewer.addPointCloud<PointT>(cloud, rgb, "cloud_points");
+	viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE,
+												3, "cloud_points");
+	viewer.setBackgroundColor (220, 220, 220);
+	while(! viewer.wasStopped())
+		viewer.spinOnce();
+}
+
+void simple_visualize(const PointCloudT::Ptr cloud, const vector<int>& indices){
+	pcl::visualization::PCLVisualizer viewer("Cloud_with_quadrilateral");
+	pcl::visualization::PointCloudColorHandlerRGBField<PointT> rgb(cloud);
+	viewer.addPointCloud<PointT>(cloud, rgb, "cloud_points");
+	viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE,
+												3, "cloud_points");
+	viewer.setBackgroundColor (220, 220, 220);
+
+	viewer.addLine(cloud->points[indices[0]], cloud->points[indices[1]], 255, 0, 0, "line1");
+	viewer.addLine(cloud->points[indices[1]], cloud->points[indices[2]], 255, 0, 0, "line2");
+	viewer.addLine(cloud->points[indices[2]], cloud->points[indices[3]], 255, 0, 0, "line3");
+	viewer.addLine(cloud->points[indices[3]], cloud->points[indices[0]], 255, 0, 0, "line4");
+
+	viewer.addSphere(cloud->points[indices[0]], 0.1, 255, 0, 0, "shphere1");
+	viewer.addSphere(cloud->points[indices[1]], 0.1, 255, 0, 0, "shphere2");
+	viewer.addSphere(cloud->points[indices[2]], 0.1, 255, 0, 0, "shphere3");
+	viewer.addSphere(cloud->points[indices[3]], 0.1, 255, 0, 0, "shphere4");
+
 	while(! viewer.wasStopped())
 		viewer.spinOnce();
 }
 
 void fill_base_points(vector<Point3D>& base_points){
-	base_points.push_back(Point3D{-6.17246, -10.3393, -1.76436});
-	base_points.push_back(Point3D{-3.31098, 17.6496, 9.53353});
-	base_points.push_back(Point3D{7.49274, 8.01208, -7.62501});
-	base_points.push_back(Point3D{-4.84244, 3.2643, 4.08124});
+	// base_points.push_back(Point3D{-5.70759, 15.0452, -2.73189});
+	// base_points.push_back(Point3D{-7.60092, 15.6629, -1.31274});
+	// base_points.push_back(Point3D{-7.62833, 15.617, -2.64937});
+	// base_points.push_back(Point3D{-1.16865, 15.0312, -0.736361});
+
+	base_points.push_back(Point3D{-6.02769, 16.0724, -5.90009});
+	base_points.push_back(Point3D{-6.48369, 16.4077, -5.28741});
+	base_points.push_back(Point3D{-6.1264, 16.1918, -5.95713});
+	base_points.push_back(Point3D{-5.95571, 16.4025, -6.48623});
+}
+
+void mark_red(PointCloudT::Ptr cloud, int index){
+	cloud->points[index].r = 230;
+	cloud->points[index].g = 0;
+	cloud->points[index].b = 0;
 }
 
 float get_distance(const PointT& point, const Point3D& base_point){
@@ -72,6 +108,8 @@ int get_nearest_point(const Point3D& base_point, PointCloudT::Ptr cloud){
 			"\tz: " << cloud->points[min_index].z << "\nwith distance = \n" <<
 			min_distance << endl;
 
+	mark_red(cloud, min_index);
+
 	return min_index;
 }
 
@@ -84,15 +122,19 @@ int main(int argc, char const *argv[]){
 	PointCloudT::Ptr cloud(new PointCloudT);
 	io::loadPCDFile<PointT> (argv[1], *cloud);
 	
-	red_colorization(cloud);
+	blue_colorization(cloud);
 	simple_visualize(cloud);
 		
 	vector<Point3D> base_points;
 	fill_base_points(base_points);
 
+	vector<int> quad_indices;
 	for(Point3D point : base_points){
-		int index = get_nearest_point(point, cloud);		
+		int index = get_nearest_point(point, cloud);
+		quad_indices.push_back(index);		
 	}
+
+	simple_visualize(cloud, quad_indices);
 
 	return 0;
 }
